ff3a89ff387f2d2d3f7e802bc250bdfbf2507aa2
Chamika12425 <sasindu0205@gmail.com>
2026-08-02 17:23:30 +0530

    Created Product List view page

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Products</h1>
+
+    <a href="" class="btn-btn-primary">Add Products</a>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="5"> No products found.</td>
+            </tr>
+        </tbody>
+    </table>
+    
+@endsection
